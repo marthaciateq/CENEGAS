06e9789b9a4699c3334847c6db8b7de49a623c37
marthaciateq <marthae.trejo@gmail.com>
2017-04-10 10:58:36 -0500

    C

---
+++ CENEGAS/js/Mi.Cenegas.js
@@ -0,0 +1,17 @@
+﻿Mi.MiTextVal = function (e, value) {
+    if ($.type(value) == 'undefined') {
+        if (e.val().trim() == '') return null
+        else return e.val().trim()
+    } else {
+        if (value == null) e.val('')
+        else e.val(value)
+    }
+}
+
+Mi.MiNumberMiVal = function (e, value, decimales, separador) {
+    if ($.type(value) == 'undefined') return Mi.Convert.stringToNumber(e.val())
+    else {
+        if (!$.isNumeric(parseFloat(value))) e.val('')
+        else e.val(Mi.Convert.numberToString(value, decimales, separador))
+    }
+}
